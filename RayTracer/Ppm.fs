0be92061fdcbf44f363ad41a22f068a8c20e1e4b
Jack Matusiewicz <jack.matusiewicz@gmail.com>
2019-10-09 23:28:59 +0100

    Initial commit with a Ppm writing function

---
+++ RayTracer/Ppm.fs
@@ -0,0 +1,37 @@
+namespace RayTracer
+
+open System.Text
+
+type Colour =
+    {
+        R : uint8
+        G : uint8
+        B : uint8
+    }
+
+[<RequireQualifiedAccess>]
+module Colour =
+
+    let toString ({R = r; G = g; B = b} : Colour) =
+        sprintf "%d %d %d" r g b
+
+type PpmFile = internal Ppm of string
+
+[<RequireQualifiedAccess>]
+module Ppm =
+
+    let toPpm (pixels : Colour [,]) : PpmFile =
+        let rows = pixels.GetLength 0
+        let cols = pixels.GetLength 1
+        let mutable sb = StringBuilder ()
+        sb <- sb.Append (sprintf "%d %d\n" rows cols)
+        for i in 0 .. rows - 1 do
+            let row =
+                pixels.[i, *]
+                |> Array.map Colour.toString
+                |> Array.fold (fun s a -> sprintf "%s %s" a s) "\n"
+            sb <- sb.Append row
+            ()
+        sb.ToString ()
+        |> Ppm
+    
